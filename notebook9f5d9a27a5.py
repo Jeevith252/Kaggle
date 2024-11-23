67cdf7c3a83c37c08791916902328e2ab8636288
Jeevith252 <jeevithkampa252@gmail.com>
2024-11-23 11:05:40 +0530

    Kaggle Notebook | notebook9f5d9a27a5 | Version 4

---
+++ notebook9f5d9a27a5.py
@@ -0,0 +1,81 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "93e83e1b",
+   "metadata": {
+    "_cell_guid": "b1076dfc-b9ad-4769-8c92-a6c4dae69d19",
+    "_uuid": "8f2839f25d086af736a60e9eeb907d3b93b6e0e5",
+    "execution": {
+     "iopub.execute_input": "2024-11-23T05:35:27.197738Z",
+     "iopub.status.busy": "2024-11-23T05:35:27.197324Z",
+     "iopub.status.idle": "2024-11-23T05:35:27.207812Z",
+     "shell.execute_reply": "2024-11-23T05:35:27.206607Z"
+    },
+    "papermill": {
+     "duration": 0.016846,
+     "end_time": "2024-11-23T05:35:27.210883",
+     "exception": false,
+     "start_time": "2024-11-23T05:35:27.194037",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Hello world\n"
+     ]
+    }
+   ],
+   "source": [
+    "print(\"Hello world\")"
+   ]
+  }
+ ],
+ "metadata": {
+  "kaggle": {
+   "accelerator": "none",
+   "dataSources": [],
+   "dockerImageVersionId": 30786,
+   "isGpuEnabled": false,
+   "isInternetEnabled": false,
+   "language": "python",
+   "sourceType": "notebook"
+  },
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.10.14"
+  },
+  "papermill": {
+   "default_parameters": {},
+   "duration": 3.188254,
+   "end_time": "2024-11-23T05:35:27.633560",
+   "environment_variables": {},
+   "exception": null,
+   "input_path": "__notebook__.ipynb",
+   "output_path": "__notebook__.ipynb",
+   "parameters": {},
+   "start_time": "2024-11-23T05:35:24.445306",
+   "version": "2.6.0"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
